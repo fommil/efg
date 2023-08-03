ac73f136d704a26f86fee2ff47d416ff77b0e87f
Sam Halliday <sam.halliday@gmail.com>
2023-08-03 11:57:32 +0100

    1 step prime reduction

--- src/main/scala/mccluskey.scala
+++ src/main/scala/mccluskey.scala
@@ -23,8 +23,9 @@ package mccluskey
 
 import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
-import scala.collection.immutable.BitSet
 import java.nio.file.Files
+
+import scala.collection.immutable.{ BitSet, ListSet }
 
 object Main {
   private val RowPattern = "^(@[_a-zA-Z0-9]+)?([ 01]+)([|][ 01]+)?$".r
@@ -67,10 +68,61 @@ object Main {
     }
     require(terms.flatMap(_.ps).distinct.length == terms.length, "labels must be unique")
 
+    val cardinality = terms.head.bits.length
+
     System.out.println(s"${terms.mkString("  ", "\n  ", "\n")}")
+
+    // calculate prime implicants by applying boolean reduction rules
+
+    val step1 = reduce_step(terms, Nil)
+    val probe = step1._1 ++ step1._2
+
+    System.out.println(s"${probe.mkString("  ", "\n  ", "\n")}")
+
+
+    // go through each column
+    // find combinations of Terms that can be merged
+
+    // iterate over the new combinations, until nothing new is discovered
+    // (we only need to check the surface at each stage, anything that was consumed as part of an earlier merge will not produce new primes)
+
+    // remove everything that is a subset (but only after it has been compared with everything)
+
+    // it feels like there might be an n-queens style search trick that would
+    // allow the invalid search to be excluded.
 
     // FIXME apply boolean reduction rules, etc
   }
+
+  // performs a single sweep of the first list of terms against themselves and
+  // the second list, returning newly merged terms and those that were not
+  // affected by the merging, separately to facilitate additional iterations.
+  //
+  // it is possible that it is more efficient to iterate based on column, rather
+  // than term, but this seems conceptually easier to understand.
+  def reduce_step(as: List[Term], bs: List[Term]): (List[Term], List[Term]) = {
+    // these should be removed for efficiency later on
+    assert(as.intersect(bs).isEmpty, s"duplicate terms in intersect: $as $bs")
+    assert(as.distinct.length == as.length, s"duplicate terms in a: $as")
+    assert(bs.distinct.length == bs.length, s"duplicate terms in b: $bs")
+
+    val combined = as ++ bs
+
+    val mergeable = for {
+      a <- as
+      b <- combined
+      if a ne b
+      if a canMerge b
+    } yield (a, b)
+
+    val eliminated = mergeable.flatMap { case (a, b) => List(a, b) }.distinct
+    val merged = mergeable.map { case (a, b) => a merge b }
+    // TODO possible that mergeable is constructed through different paths,
+    // leading to dupes that we need to handle here (only appearing in _1).
+
+    ((combined diff eliminated), merged)
+  }
+
 }
 
 // a potential prime implicant, derived from one or more p-terms
@@ -82,6 +134,31 @@ case class Term(
 ) {
   require(bits.nonEmpty)
   require(ps.nonEmpty)
+
+  // if a sequence of bits matches this, then they also match that.
+  def subsetOf(that: Term): Boolean =
+    bits.zip(that.bits).forall {
+      case (Some(_), None) => true
+      case (oa, ob) => oa == ob
+    }
+
+  def canMerge(that: Term): Boolean = {
+    val alts = bits.zip(that.bits).filter {
+      case (Some(oa), Some(ob)) => oa != ob
+      case _ => false
+    }
+    alts.length == 1
+  }
+
+  // does not check for compatibility, always guard with canMerge
+  def merge(that: Term): Term = {
+    val bits_ = bits.zip(that.bits).map {
+      case (Some(oa), Some(ob)) if oa == ob => Some(oa)
+      case _ => None
+    }
+    val ps_ = ps ++ that.ps
+    Term(bits_, ps_)
+  }
 
   override def toString: String = {
     val input = new String(bits.map {
