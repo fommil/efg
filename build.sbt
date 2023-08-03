7b575c1f15d215cd9bb7b974c088c1ab1b469f92
Sam Halliday <sam.halliday@gmail.com>
2023-08-03 15:29:05 +0100

    all prime implicants

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "electronics"
 
 licenses := List(License.GPL3_or_later)
 ThisBuild / scalaVersion := "2.13.11"
+
+scalacOptions += "-Ywarn-unused"
--- src/main/scala/mccluskey.scala
+++ src/main/scala/mccluskey.scala
@@ -25,135 +25,139 @@ import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.Files
 
-import scala.collection.immutable.{ BitSet, ListSet }
+import scala.collection.immutable.ArraySeq
 
 object Main {
   private val RowPattern = "^(@[_a-zA-Z0-9]+)?([ 01]+)([|][ 01]+)?$".r
-  private def parseBits(s: String): Seq[Boolean] = s.replace(" ", "").map {
-    case '1' => true
-    case '0' => false
-    // protected by regexps
-  }
 
   def main(args: Array[String]): Unit = {
     require(args.length == 1, "one input file must be provided")
     val file = new File(args(0))
     require(file.isFile(), s"$file must exist")
+    val input = Files.readString(file.toPath, UTF_8)
 
-    // construct the canonical representation
-    val canon = Files.readString(file.toPath, UTF_8)
+    val canon = canonical_representation(input)
+    val primes = prime_implicants(canon)
+
+    System.out.println(s"${primes.mkString("  ", "\n  ", "\n")}")
+
+    // FIXME calculate minimum sum of prime implicants
+  }
+
+  // construct the canonical representation from the user's .truth table
+  def canonical_representation(s: String): List[Term] = {
+    def parseBits(s: String): Seq[Boolean] = s.replace(" ", "").map {
+      case '1' => true
+      case '0' => false
+    }
+
+    val rows = s
       .split("\n").toList
       .flatMap { line =>
         val row = line.split("#")(0)
         if (row.trim.isEmpty) None
         else row match {
-          case RowPattern(label, input, null) => Some((parseBits(input), List(1), Option(label)))
-          case RowPattern(label, input, output) => Some((parseBits(input), parseBits(output.tail), Option(label)))
+          case RowPattern(label, input, null) => Some((parseBits(input), List(true), Option(label)))
+          case RowPattern(label, input, output) =>
+            val out = parseBits(output.tail)
+            if (out.isEmpty || out.forall(!_)) None
+            else Some((parseBits(input), out, Option(label)))
         }
       }
-      .filterNot(_._2.toList.distinct == List(false))
 
-    require(canon.map(_._1.length).distinct.length == 1, "inputs must have the same length")
-    require(canon.map(_._2.length).distinct.length == 1, "outputs must have the same length")
-    require(canon.distinct.length == canon.length, "duplicates not allowed")
+    require(rows.map(_._1.length).distinct.length == 1, "inputs must have the same length")
+    require(rows.map(_._2.length).distinct.length == 1, "outputs must have the same length")
+    require(rows.distinct.length == rows.length, "duplicates not allowed")
 
-    require(canon.forall(_._2.length == 1), "only one output allowed") // TODO support multiple outputs
+    require(rows.forall(_._2.length == 1), "only one output allowed") // TODO support multiple outputs
 
-    // System.out.println(s"$canon")
-
-    val terms = canon.zipWithIndex.map {
+    val terms = rows.zipWithIndex.map {
       case ((input, _, label_), i) =>
         val label = label_.map(_.tail).getOrElse(i.toString)
-        Term(input.map(Some(_)).toArray, List(label))
+        Term(input.map(Some(_)).to(ArraySeq), List(label))
     }
     require(terms.flatMap(_.ps).distinct.length == terms.length, "labels must be unique")
 
-    val cardinality = terms.head.bits.length
-
-    System.out.println(s"${terms.mkString("  ", "\n  ", "\n")}")
-
-    // calculate prime implicants by applying boolean reduction rules
-
-    val step1 = reduce_step(terms, Nil)
-    val probe = step1._1 ++ step1._2
-
-    System.out.println(s"${probe.mkString("  ", "\n  ", "\n")}")
-
-
-    // go through each column
-    // find combinations of Terms that can be merged
-
-    // iterate over the new combinations, until nothing new is discovered
-    // (we only need to check the surface at each stage, anything that was consumed as part of an earlier merge will not produce new primes)
-
-    // remove everything that is a subset (but only after it has been compared with everything)
-
-    // it feels like there might be an n-queens style search trick that would
-    // allow the invalid search to be excluded.
-
-    // FIXME apply boolean reduction rules, etc
+    terms
   }
 
-  // performs a single sweep of the first list of terms against themselves and
-  // the second list, returning newly merged terms and those that were not
-  // affected by the merging, separately to facilitate additional iterations.
-  //
-  // it is possible that it is more efficient to iterate based on column, rather
-  // than term, but this seems conceptually easier to understand.
-  def reduce_step(as: List[Term], bs: List[Term]): (List[Term], List[Term]) = {
-    // these should be removed for efficiency later on
-    assert(as.intersect(bs).isEmpty, s"duplicate terms in intersect: $as $bs")
-    assert(as.distinct.length == as.length, s"duplicate terms in a: $as")
-    assert(bs.distinct.length == bs.length, s"duplicate terms in b: $bs")
+  // calculates the unique prime implicants from the canonical representation
+  def prime_implicants(terms: List[Term]): List[Term] = {
+    // performs a single sweep of the first list of terms against themselves and
+    // the second list, returning newly merged terms followed by those that were
+    // not affected by the merging.
+    //
+    // it is possible that it is more efficient to iterate based on column, rather
+    // than term, but this seems conceptually easier to understand.
+    def step(as: List[Term], bs: List[Term]): (List[Term], List[Term]) = {
+      // assertions can be removed when we're confident that bugs have been found
+      assert(as.intersect(bs).isEmpty, s"duplicate terms in intersect: $as $bs")
+      assert(as.distinct.length == as.length, s"duplicate terms in a: $as")
+      assert(bs.distinct.length == bs.length, s"duplicate terms in b: $bs")
 
-    val combined = as ++ bs
+      val combined = as ++ bs
 
-    val mergeable = for {
-      a <- as
-      b <- combined
-      if a ne b
-      if a canMerge b
-    } yield (a, b)
+      val mergeable = for {
+        a <- as
+        b <- combined.dropWhile(_ ne a)
+        if a ne b
+        if a canMerge b
+      } yield (a, b)
 
-    val eliminated = mergeable.flatMap { case (a, b) => List(a, b) }.distinct
-    val merged = mergeable.map { case (a, b) => a merge b }
-    // TODO possible that mergeable is constructed through different paths,
-    // leading to dupes that we need to handle here (only appearing in _1).
+      // we can arrive at merged results through multiple paths, but always at the
+      // same step of the iteration, so we must distinct by the _.bits
+      val eliminated = mergeable.flatMap { case (a, b) => List(a, b) }.distinct
+      val merged = mergeable.map { case (a, b) => a merge b }.distinctBy(_.bits)
 
-    ((combined diff eliminated), merged)
+      // the diff here should be using .bits as the comparison, but since new
+      // terms only enter from merging it is not strictly needed. Distinct here is
+      // just to avoid duplicate checks.
+      val unchanged = combined diff eliminated
+      (merged, unchanged)
+    }
+
+    var surface = (terms, List.empty[Term])
+    while (surface._1.nonEmpty) {
+      // System.out.println(s"==== STEP ====")
+      // System.out.println(s"${surface._1.mkString("  ", "\n  ", "\n")}")
+      // System.out.println(s"${surface._2.mkString("  ", "\n  ", "\n")}")
+
+      surface = step(surface._1, surface._2)
+    }
+    surface._1 ++ surface._2
   }
 
 }
 
 // a potential prime implicant, derived from one or more p-terms
 case class Term(
-  // None is McCluskey's hyphen
-  bits: Array[Option[Boolean]],
+  // input bits, None is McCluskey's hyphen
+  bits: ArraySeq[Option[Boolean]],
   // the row labels included in this term
   ps: List[String]
 ) {
   require(bits.nonEmpty)
   require(ps.nonEmpty)
 
-  // if a sequence of bits matches this, then they also match that.
-  def subsetOf(that: Term): Boolean =
-    bits.zip(that.bits).forall {
-      case (Some(_), None) => true
-      case (oa, ob) => oa == ob
-    }
+  // // if a sequence of bits matches this, then they also match that.
+  // def subsetOf(that: Term): Boolean =
+  //   bits.zip(that.bits).forall {
+  //     case (Some(_), None) => true
+  //     case (oa, ob) => oa == ob
+  //   }
 
   def canMerge(that: Term): Boolean = {
     val alts = bits.zip(that.bits).filter {
-      case (Some(oa), Some(ob)) => oa != ob
-      case _ => false
+      // case (Some(a), Some(b)) => a != b
+      case (oa, ob) => oa != ob
     }
-    alts.length == 1
+    alts.lengthCompare(1) == 0
   }
 
   // does not check for compatibility, always guard with canMerge
   def merge(that: Term): Term = {
     val bits_ = bits.zip(that.bits).map {
-      case (Some(oa), Some(ob)) if oa == ob => Some(oa)
+      case (Some(a), Some(b)) if a == b => Some(a)
       case _ => None
     }
     val ps_ = ps ++ that.ps
@@ -161,13 +165,13 @@ case class Term(
   }
 
   override def toString: String = {
-    val input = new String(bits.map {
+    val input = bits.map {
       case None => '-'
       case Some(true) => '1'
       case Some(false) => '0'
-    })
+    }
     val indexes = ps.mkString("(", ", ", ")")
-    s"$input $indexes"
+    s"${input.mkString} $indexes"
   }
 }
 
