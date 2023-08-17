9add84f3f44f7ecf47cb71cd0258255d3206be98
Sam Halliday <sam.halliday@gmail.com>
2023-08-17 20:45:33 +0100

    try to add deMorgan

--- src/main/scala/logic.scala
+++ src/main/scala/logic.scala
@@ -37,8 +37,8 @@ sealed trait Logic {
       else parts.mkString("(", " + ", ")")
   }
 
-  def render(show: Int => String): String = render(false)(show)
-  def render: String = render(false)(_.toString)
+  def render(show: Int => String): String = render(true)(show)
+  def render: String = render(true)(_.toString)
 
   //  override final def toString: String = render(false)(_.toString)
 
@@ -49,10 +49,61 @@ sealed trait Logic {
     case Or(os) => os.exists(_.eval(input))
   }
 
+  private def deMorgan: Logic = this match {
+    case And(entries) =>
+      val (invs, regs) = entries.partitionMap {
+        case Inv(e) => Left(e)
+        case e => Right(e)
+      }
+      if (invs.size > regs.size) {
+        // ~A.~B.C = ~(A + B + ~C)
+        Inv(Or(invs ++ regs.map(Inv(_)))).factor
+      } else this
+
+    case Or(entries) =>
+      val (invs, regs) = entries.partitionMap {
+        case Inv(e) => Left(e)
+        case e => Right(e)
+      }
+      if (invs.size > regs.size) {
+        // ~(~A + ~B + C) = A.B.~C
+        Inv(And(invs ++ regs.map(Inv(_)))).factor
+      } else this
+
+    case other => other
+  }
+
+  // NOTE: this is probably a really rubbish approach. What we really want to do
+  // is to create the list of all the unique nodes, and then generate
+  // substitutions that we then search through. This is just blindly applying
+  // rules without any regard for where we are going. But it's useful for
+  // testing.
   // extracts common factors using a greedy algorithm
   def factor: Logic = this match {
     case In(_) => this
-    case Inv(a) => Inv(a.factor)
+    case Inv(a) =>
+      // this should only flip if *everything* is inverted, to avoid infinite
+      // loops with deMorgans.
+      a.factor match {
+        case Inv(e) => e
+        case a@ And(entries) =>
+          val (invs, regs) = entries.partitionMap {
+            case Inv(e) => Left(e)
+            case other => Right(other)
+          }
+          if (regs.isEmpty) And(invs)
+          else Inv(a)
+        case a@ Or(entries) =>
+          val (invs, regs) = entries.partitionMap {
+            case Inv(e) => Left(e)
+            case other => Right(other)
+          }
+          if (regs.isEmpty) Or(invs)
+          else Inv(a)
+
+        case other => Inv(other)
+      }
+
     case And(entries) =>
       // this is basically PofS in McCluskey, but for a more general AST.
       val (tops, other) = entries.map(_.factor).partitionMap {
@@ -66,7 +117,8 @@ sealed trait Logic {
         case Or(ors) => Util.overlaps(ors, tops)
         case _ => false
       }
-      And(tops.flatten ++ remain)
+      And(tops.flatten ++ remain).deMorgan
+
     case Or(entries) =>
       val parts = entries.map(_.factor)
       val (popular, many) = parts.toList.flatMap {
@@ -76,26 +128,29 @@ sealed trait Logic {
         case (expr, occs) => expr -> occs.size
       }.maxBy(_._2) // this is the greedy selection
 
-      if (many < 2) {
-        Or(parts)
-      } else {
-        val (factored, uncommon) = parts.partitionMap {
-          case e @ And(es) =>
-            if (es.contains(popular)) Left(And(es - popular)) else Right(e)
-          case e =>
-            if (e == popular) Left(e) else Right(e)
-        }
-
-        val and = And(Set(popular) + Or(factored))
-
-        if (uncommon.isEmpty) and
-        else {
-          Or(uncommon).factor match {
-            case Or(nested) => Or(nested + and)
-            case other => Or(Set(and, other))
+      {
+        if (many < 2) {
+          Or(parts)
+        } else {
+          val (factored, uncommon) = parts.partitionMap {
+            case e @ And(es) =>
+              if (es.contains(popular)) Left(And(es - popular)) else Right(e)
+            case e =>
+              if (e == popular) Left(e) else Right(e)
           }
-        }
+
+          val and = And(Set(popular) + Or(factored))
+
+          if (uncommon.isEmpty) and
+          else {
+            Or(uncommon).factor match {
+              case Or(nested) => Or(nested + and)
+              case other => Or(Set(and, other))
+            }
+          }
+        }.deMorgan
       }
+
   }
 
   // maybe we should allow this to be mutated...
@@ -148,7 +203,17 @@ object Logic {
     def apply(entries: Set[Logic]): Logic = {
       require(entries.nonEmpty)
       if (entries.size == 1) entries.head
-      else new And(entries)
+      else {
+        // this is a bit much, will backfire when we try to share gates. Maybe
+        // all constructions should be followed by a choice to leave as-is or to
+        // try to unnest.
+        val (nested, not) = entries.partitionMap {
+          case And(es) => Left(es)
+          case other => Right(other)
+        }
+        if (not.isEmpty) new And(nested.flatten)
+        else new And(entries)
+      }
     }
   }
 
@@ -156,7 +221,16 @@ object Logic {
     def apply(entries: Set[Logic]): Logic = {
       require(entries.nonEmpty)
       if (entries.size == 1) entries.head
-      else new Or(entries)
+      else {
+        val (nested, not) = entries.partitionMap {
+          case Or(es) => Left(es)
+          case other => Right(other)
+        }
+        if (not.isEmpty)
+          new Or(nested.flatten)
+        else
+          new Or(entries)
+      }
     }
   }
 
@@ -177,7 +251,7 @@ object Main {
       case Right(as) => as
     }
 
-    val syms = Util.alpha.take(mins.input_width).zipWithIndex.map(_.swap).toMap
+    val syms = Util.alpha.take(mins.input_width).map(_.toLowerCase).zipWithIndex.map(_.swap).toMap
 
     // output some really simple deduped gate counts (not really a cost)
     // this only works for 2 outputs, make it general
@@ -197,7 +271,8 @@ object Main {
         case _ => true
       }
 
-      System.out.println(s"nodes = ${nodes.size} (${af.render(syms)} ; ${bf.render(syms)})")
+      // FIXME the output is junk, one of the rules is clearly buggy
+      System.out.println(s"nodes = ${nodes.size} { ${af.render(syms)} ; ${bf.render(syms)} }")
     }
   }
 }
